487eaa6d1bf4476d45afb8fbb1b96cb8bfe986d2
Aliaksandr Martyniuk <banifest@gmail.com>
2019-05-30 11:08:42 +0300

    Add tables for hana db

--- service/server.js
+++ service/server.js
@@ -1,11 +1,11 @@
 /*eslint no-console: 0, no-unused-vars: 0*/
 "use strict";
 
-var xsjs = require("@sap/xsjs");
-var xsenv = require("@sap/xsenv");
-var port = process.env.PORT || 3000;
+const xsjs = require("@sap/xsjs");
+const xsenv = require("@sap/xsenv");
+const port = process.env.PORT || 3000;
 
-var options = {
+let options = {
     anonymous: true, // remove to authenticate calls
     redirectUrl: "/index.xsjs"
 };
